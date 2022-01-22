a7209c279514a05fb8c336496fb375e9605cb3ad
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2022-01-21 21:04:59 -0500

    style: date display

--- components/blog/AuthorSection.vue
+++ components/blog/AuthorSection.vue
@@ -1,16 +1,28 @@
 <template>
-    <section class="justify-between block max-w-xl mx-auto my-12 space-x-10 space-y-4 md:flex">
-        <div v-if="author" class="flex items-center justify-center whitespace-nowrap">
-            <img v-if="author.avatar" :src="author.avatar" class="w-20 h-20 my-0 mr-4 rounded-full">
+    <section
+        class="justify-between block max-w-xl mx-auto my-12 space-x-10 space-y-4  md:flex"
+    >
+        <div
+            v-if="author"
+            class="flex items-center justify-center whitespace-nowrap"
+        >
+            <img
+                v-if="author.avatar"
+                :src="author.avatar"
+                class="w-20 h-20 my-0 mr-4 rounded-full"
+            />
             <div class="text-left">
                 <div>
-                    <a class="text-xl font-bold no-underline link hover:underline" :href="author.website">
+                    <a
+                        class="text-xl font-bold no-underline  link hover:underline"
+                        :href="author.website"
+                    >
                         {{ author.name }}
                     </a>
                 </div>
                 <span class="text-gray-500">
-                    <span v-if="post.updatedAt">Updated on</span>
-                    <time datetime="">{{ post_date }}</time>
+                    <!-- <span v-if="post.updatedAt">Updated on</span> -->
+                    <time datetime="">on {{ post_date }}</time>
                 </span>
             </div>
         </div>
@@ -20,39 +32,48 @@
                 <!-- <time datetime="{{ date format="Y-m-d" }}">{{ date format="F jS, Y" }}</time> -->
             </div>
         </div>
-        
+
         <div v-if="post.tags" class="place-content-end">
-            <span v-for="tag in $utils.tagSpliter(post.tags)" :key="tag" class="inline-block mb-1 space-x-0">
-                <nuxt-link :to="tag_url(tag)" class="text-base no-underline tag indigo">
+            <span
+                v-for="tag in $utils.tagSpliter(post.tags)"
+                :key="tag"
+                class="inline-block mb-1 space-x-0"
+            >
+                <nuxt-link
+                    :to="tag_url(tag)"
+                    class="text-base no-underline tag indigo"
+                >
                     #{{ tag }}
                 </nuxt-link>
             </span>
         </div>
-        
     </section>
 </template>
 
 <script>
 export default {
-    name: 'AuthorSection',
+    name: "AuthorSection",
     props: {
         author: false,
         post: false,
         path: {
             type: String,
-            default: '/blog',
-        }
+            default: "/blog",
+        },
     },
     methods: {
-        tag_url( tag ){
+        tag_url(tag) {
             return `${this.path}/tag/${tag}`;
-        }
+        },
     },
-    computed:{
-        post_date(){
-			let date =  this.post.updatedAt || this.post.date || this.post.createdAt; 
-			return new Date(date).toLocaleString("en-US", { dateStyle: 'medium' });
-		},
+    computed: {
+        post_date() {
+            let date =
+                this.post.date || this.post.updatedAt || this.post.createdAt;
+            return new Date(date).toLocaleString("en-US", {
+                dateStyle: "medium",
+            });
+        },
     },
-}
+};
 </script>
--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,17 +1,23 @@
 <template>
-	<article class="max-w-6xl px-3 mx-auto">
-		<header class="max-w-4xl mx-auto text-center article">
-			<h1><nuxt-link :to="url" class="text-4xl text-gray-800 no-underline dark:text-gray-300 md:text-6xl">{{ post.title }}</nuxt-link></h1>
-			<p v-if="post.intro">{{ post.intro }}</p>	
-		</header>
+    <article class="max-w-6xl px-3 mx-auto">
+        <header class="max-w-4xl mx-auto text-center article">
+            <h1>
+                <nuxt-link
+                    :to="url"
+                    class="text-4xl text-gray-800 no-underline  dark:text-gray-300 md:text-6xl"
+                    >{{ post.title }}</nuxt-link
+                >
+            </h1>
+            <p v-if="post.intro">{{ post.intro }}</p>
+        </header>
 
-		<img v-if="image" class="rounded-xl" :src="image" :alt="post.title">
-		
-		<author-section :post="post" :author="author" :path="'/articles'" />
+        <img v-if="image" class="rounded-xl" :src="image" :alt="post.title" />
 
-		<nuxt-content :document="post" class="max-w-3xl mx-auto article" />
+        <author-section :post="post" :author="author" :path="'/articles'" />
 
-			<!-- {{ main_content }}
+        <nuxt-content :document="post" class="max-w-3xl mx-auto article" />
+
+        <!-- {{ main_content }}
 				{{ if type == "text" }}
 					{{ text }}
 				{{ elseif type == "code_block" }}
@@ -24,90 +30,91 @@
 				{{ /if }}
 			{{ /main_content }} -->
 
-		<!-- <article-subscribe-form /> -->
-		<newsletter-signup-form />
-
-	</article>
+        <!-- <article-subscribe-form /> -->
+        <newsletter-signup-form />
+    </article>
 </template>
 
 <script>
-import ArticleSubscribeForm from '~/components/ArticleSubscribeForm.vue';
-import AuthorSection from '~/components/blog/AuthorSection.vue';
-import NewsletterSignupForm from '~/components/NewsletterSignupForm.vue';
+import ArticleSubscribeForm from "~/components/ArticleSubscribeForm.vue";
+import AuthorSection from "~/components/blog/AuthorSection.vue";
+import NewsletterSignupForm from "~/components/NewsletterSignupForm.vue";
 
 export default {
-	components: {
-		NewsletterSignupForm,
-		AuthorSection,
-		ArticleSubscribeForm
-	},
-	async asyncData({ params, error, $content }) {
-		try {
-			// const postPath = `/articles/${params.slug}`;
-			const [post] = await $content("articles", { deep: true })
-				.where({ slug: params.slug })
-				.fetch();
-			return { post };
-		} catch (err) {
-			error({
-				statusCode: 404,
-				message: "Page could not be found",
-			});
-		}
-	},
-    methods: {
-        tag_url( tag ){
-            return `/articles/tag/${tag}`;
+    components: {
+        NewsletterSignupForm,
+        AuthorSection,
+        ArticleSubscribeForm,
+    },
+    async asyncData({ params, error, $content }) {
+        try {
+            // const postPath = `/articles/${params.slug}`;
+            const [post] = await $content("articles", { deep: true })
+                .where({ slug: params.slug })
+                .fetch();
+            return { post };
+        } catch (err) {
+            error({
+                statusCode: 404,
+                message: "Page could not be found",
+            });
         }
     },
-	computed:{
-		url(){
+    methods: {
+        tag_url(tag) {
+            return `/articles/tag/${tag}`;
+        },
+    },
+    computed: {
+        url() {
             return `/articles/${this.post.slug}`;
         },
-		image(){
+        image() {
             let image = this.post.hero_image || this.post.image;
-			if ( !image )
-				return false;
+            if (!image) return false;
             // remove '/static/' from the path
-            if ( image.substring(0, 8) != '/static/' )
-                image = image.substring(7);
+            if (image.substring(0, 8) != "/static/") image = image.substring(7);
             // force add '/media/' if not already there
-            if ( image.substring(0, 7) != '/media/' )
-                image = `/media/${image}`;
+            if (image.substring(0, 7) != "/media/") image = `/media/${image}`;
             return image;
         },
-		author(){
-			return {
-				name: 'Nick Frostbutter',
-				website: 'https://frostbutter.com',
-				avatar: '/img/nick.jpg',
-			};
-			// return false;
-		},
-	},
+        author() {
+            return {
+                name: "Nick Frostbutter",
+                website: "https://frostbutter.com",
+                avatar: "/img/nick.jpg",
+            };
+            // return false;
+        },
+    },
     head() {
-		return this.$seo({
-			templateTitle: this.post.template || '%title% - %name%',
-			title: this.post.title || 'Articles',
-			description: this.post.description || this.post.intro || 'Checkout this awesome article. It is super good. You should read it :)',
-			canonical: this.post.canonical || 'auto',
-			openGraph: {
-				image: {
-					url: this.image ? `https://frostbutter.com${this.image}` : 'https://frostbutter.com/img/nick.jpg',
-				},
-			},
-			twitter: {
-				card: this.image ? 'summary_large_image' : 'summary',
-				// url: '/',
-				// title: '<local page title>',
-				// description: '<local page desc>',
-				// image: '<local page image>',
-			}, 
-		})
-	},
+        return this.$seo({
+            templateTitle: this.post.template || "%title% - %name%",
+            title: this.post.title || "Articles",
+            description:
+                this.post.description ||
+                this.post.intro ||
+                "Checkout this awesome article. It is super good. You should read it :)",
+            canonical: this.post.canonical || "auto",
+            openGraph: {
+                image: {
+                    url: this.image
+                        ? `https://frostbutter.com${this.image}`
+                        : "https://frostbutter.com/img/nick.jpg",
+                },
+            },
+            twitter: {
+                card: this.image ? "summary_large_image" : "summary",
+                // url: '/',
+                // title: '<local page title>',
+                // description: '<local page desc>',
+                // image: '<local page image>',
+            },
+        });
+    },
 };
 </script>
 
 <style lang="postcss">
-@import '~/assets/css/article.css'
+@import "~/assets/css/article.css";
 </style>
